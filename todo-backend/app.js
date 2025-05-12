f9f401acfb833df151238448db54c8f6c5150b2d
Dhruva Keshav Arya <dhruvaa866@gmail.com>
2025-05-12 15:07:43 +0530

    Update app.js

--- todo-backend/app.js
+++ todo-backend/app.js
@@ -23,7 +23,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: false,
+    secure: true,
+    sameSite: "none",
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24, // 1 day
   }
